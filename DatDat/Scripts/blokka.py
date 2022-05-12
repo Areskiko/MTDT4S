6131dc717c07ce3aa6d7ca1284fcdbad0e35c62b
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-05-12 10:58:00 +0200

    Block craft 3d

---
+++ DatDat/Scripts/blokka.py
@@ -0,0 +1,18 @@
+# Compute the minimum block accesses needed to join two tables
+
+def min_blocks(tab1_size, tab2_size, slots):
+    """Compute the minimum number of blocks needed to join two tables"""
+
+    mi = min(tab1_size, tab2_size)
+    ma = max(tab1_size, tab2_size)
+
+    rounds = ceil(mi/(slots-2))
+    iter_per_round = ma
+
+    return mi + rounds * iter_per_round
+
+if __name__ == "__main__":
+    t1 = int(input("Table 1 blocks: "))
+    t2 = int(input("Table 2 blocks: "))
+    sl = int(input("Memory blocks: "))
+    print(min_blocks(t1, t2, sl))
