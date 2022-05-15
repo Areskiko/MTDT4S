e50ccebbaa75d3fd2ecc6b5c54e1fee6974d553c
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2022-05-15 10:34:12 +0200

    buddy

--- OS/buddy_algo.py
+++ OS/buddy_algo.py
@@ -20,15 +20,18 @@ for i in range(len(input_list)):
     exponent = math.ceil(math.log2(input_list[i]))
     size_of_process = 2 ** exponent
     number_of_claimed_blocks = math.ceil(size_of_process / size_of_block)
-    if i == 0:
-        for i in range(number_of_claimed_blocks):
-            memory_list[i] = 1
+    flag = -1
+    for j in range(len(memory_list)):
+        if memory_list[j] == 0 and check_can_set(number_of_claimed_blocks, memory_list, j) and j*size_of_block % size_of_process == 0:
+            flag = j
+            for k in range(j, j+number_of_claimed_blocks):
+                memory_list[k] = 1
+            break
+    if flag == -1:
+        print("Not enough memory for process", a)
     else:
-        for j in range(len(memory_list)):
-            if memory_list[j] == 0 and check_can_set(number_of_claimed_blocks, memory_list, j) and j*size_of_block % size_of_process == 0:
-                for k in range(j, j+number_of_claimed_blocks):
-                    memory_list[k] = 1
-                break
+        print("Process", a, "is allocated at", flag*size_of_block, "with size", size_of_process)
+
 
 
 print("\nMemory List: ", memory_list)
