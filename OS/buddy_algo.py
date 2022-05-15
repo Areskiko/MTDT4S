97b5da93d74f55c4a67f0308a114dc66a2b5297d
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2022-05-15 09:52:51 +0200

    inputs

--- OS/buddy_algo.py
+++ OS/buddy_algo.py
@@ -1,1 +1,10 @@
 import math
+
+
+input_list = []
+alloc = int(input("Memory available: "))
+number_of_processes = int(input("Enter the number of allocations: "))
+
+for i in range(number_of_processes):
+    input_list.append(int(input("Enter memory demand: ")))
+
