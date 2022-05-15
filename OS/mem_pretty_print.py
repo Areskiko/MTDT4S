7ea47e53aa86f7779fd4c875ef72b1d0c067baf4
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-05-15 11:07:48 +0200

    pp

---
+++ OS/mem_pretty_print.py
@@ -0,0 +1,20 @@
+#list[list[float]]
+def pp(frames) -> None:
+    """
+    Pretty print the memory frames.
+    """
+
+    print("-"*(len(frames[0])*5))
+    for (i, frame) in enumerate(frames):
+        print('Frame{}'.format(i), end='')
+        for value in frame:
+            value = value if value != None else 'X'
+            print(' |{:>2}'.format(value), end='')
+        print()
+        print("-"*(len(frame)*5))
+
+f1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 4, 4]
+f2 = [None, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+f3 = [None, None, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5]
+
+pp([f1, f2, f3])
